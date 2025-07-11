68792db30a2afc5698518cabc8fa4eaab6a2bd13
talen <talen3031@gmail.com>
2025-07-12 04:08:26 +0800

    update CICD flow and env

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -8,7 +8,7 @@ from models import db,User,Category
 
 @pytest.fixture
 def app():
-    db_name = os.environ.get('TEST_DB_NAME', 'Ecommerce_test')   # 預設本地用 Ecommerce_test/CI使用Ecommerce
+    db_name = os.environ.get('TEST_DB_NAME', 'Ecommerce_test') 
     db_uri = f'postgresql://postgres:talen168168@localhost:5432/{db_name}'
 
     app = create_app({
