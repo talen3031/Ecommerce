7bc709f988f57923b1e7fa1ec58d068320ac8f41
talen <talen3031@gmail.com>
2025-07-12 19:26:16 +0800

    change frontend and launch forgetpassword service2

--- backend/etl.py
+++ backend/etl.py
@@ -6,7 +6,10 @@ from app import create_app
 
 app = create_app()
 app.app_context().push()
-
+if Category.query.first():
+    print("🟢 已有資料，跳過 ETL 載入")
+    exit(0)
+    
 # 1. 載入 categories
 def etl_categories():
     print("載入 categories.json ...")
