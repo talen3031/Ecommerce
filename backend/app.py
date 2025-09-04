edf9639078759d00049e5d3eb52b381e89b7df85
talen <talen3031@gmail.com>
2025-09-04 15:29:40 +0800

    update ngnix(backend)2

--- backend/app.py
+++ backend/app.py
@@ -141,7 +141,7 @@ def create_app(config_name=None, test_config=None):
 
     @app.route('/')
     def index():
-        return "Nerd.com API is running!"
+        return "Welcometo nerd.com backend! \n Nginx is listening http://0.0.0.0:8080 \n API is running on http://127.0.0.1:8000 \n socketio is running on http://127.0.0.1:8001"
 
     return app
 app = create_app() 
